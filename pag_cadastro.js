e6aca686a7bf692e24185420f4708c574bbf8bdd
Ellen <107266328+Izillen@users.noreply.github.com>
2022-08-26 07:52:14 -0300

    Escola

---
+++ pag_cadastro.js
@@ -0,0 +1,90 @@
+const form = document.getElementById('form')
+const username = document.getElementById('validationCustomName')
+const email = document.getElementById('validationCustomEmail')
+const senha = document.getElementById('validationCustomSenha')
+const conf_senha = document.getElementById('validationCustomConfirm')
+const checkBox = document.getElementById('termsCheck')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const senhaValue = senha.value.trim()
+    const conf_senhaValue = conf_senha.value.trim()
+    const checkBoxValue = checkBox.value.trim()
+
+    if(usernameValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(username, 'Preencha esse campo')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(username)
+    }
+
+    if(emailValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(email)
+    }
+   
+    if(senhaValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(senha, 'Preencha esse campo')
+
+    } else if(senhaValue.length < 6) { 
+        setErrorFor(senha, 'Senha deve ter mais que 6 caracteres')
+    }else {
+        // adicionar a classe de sucesso
+        setSuccessFor(senha)
+    }
+
+    if(conf_senhaValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(conf_senha, 'Preencha esse campo')
+
+    } else if(senhaValue !== conf_senhaValue) { 
+        setErrorFor(conf_senha, 'Senhas não tão iguais')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(conf_senha)
+    }
+    if (checkBox.checked){
+        setSuccessFor(checkBox)
+    }else{
+        setErrorFor(checkBox, 'Você deve concordar com os termos para entrar')
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
