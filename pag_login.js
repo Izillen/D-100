fd78ee20e05972e9f3c1c02785a6dcc7a4decba7
Ellen <107266328+Izillen@users.noreply.github.com>
2022-08-30 08:42:16 -0300

    Escola

--- pag_login.js
+++ pag_login.js
@@ -1,20 +1,51 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+const form = document.getElementById('form');
+const username = document.getElementById('validationCustomName');
+const senha = document.getElementById('validationCustomSenha');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+});
+
+function checkInputs() {
+
+    const usernameValue = username.value.trim()
+    const senhaValue = senha.value.trim()
+
+    if(usernameValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(username, 'Preencha esse campo')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(username)
+    }
+   
+    if(senhaValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(senha, 'Preencha esse campo')
+
+    } else if(senhaValue.length < 6) { 
+        setErrorFor(senha, 'Senha deve ter mais que 6 caracteres')
+    }else {
+        // adicionar a classe de sucesso
+        setSuccessFor(senha)
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
